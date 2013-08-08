d2a3ab1a263ef1148de54a1545eecfef74a54cb1
Nicholas Chen <nchen.dev@mac.com>
2013-08-07 23:20:59 -0700

    Simplified version of server using connect

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
-var http = require('http');
-var fs = require('fs');
-var url = require('url');
-var path = require('path');
+var connect = require('connect'), 
+http = require('http');
 
-var server = http.createServer(function(req, res) {
-
+var app = connect()
+.use(connect.favicon())
+.use(connect.logger('dev'))
+.use(connect.static(__dirname))
+.use(connect.cookieParser())
+.use(function(req, res){
   if(req.url === '/endpoint1') {
     res.writeHead(200, {
       'Content-Type': 'application/json'
@@ -14,51 +16,11 @@ var server = http.createServer(function(req, res) {
     res.writeHead(200, {
       'Content-Type': 'application/json'
     });
-    res.end(JSON.stringify(endPoint2())); 
-  } else { // Default to serving files
-
-    var contentTypesByExtension = {
-      '.html': "text/html",
-      '.css':  "text/css",
-      '.js':   "text/javascript"
-    };
-
-    var uri = url.parse(req.url).pathname;
-    var filename = path.join(process.cwd(), uri);
-
-    path.exists(filename, function(exists) {
-      if(!exists) {
-        res.writeHead(404, {"Content-Type": "text/plain"});
-        res.write("404 Not Found\n");
-        res.end();
-        return;
-      }
-      if (fs.statSync(filename).isDirectory()) filename += 'index.html';
-
-      fs.readFile(filename, 'binary', function(err, file) {
-        if(err) {
-          res.writeHead(500, {
-            'Content-Type': 'text/plain'
-          });
-          res.write(err + '\n');
-          res.end();
-          return;
-        }
-
-        console.log(filename);
-        var headers = {};
-        var contentType = contentTypesByExtension[path.extname(filename)];
-        if (contentType) headers["Content-Type"] = contentType;
-        res.writeHead(200, headers);
-        res.write(file, "binary");
-        res.end();
-      });
-    });
+    res.end(JSON.stringify(endPoint2()));
   }
 });
 
-server.listen(8080, "127.0.0.1");
-console.log('Server running at http://127.0.0.1:8080/');
+http.createServer(app).listen(3000)
 
 function endPoint1() {
   return {
